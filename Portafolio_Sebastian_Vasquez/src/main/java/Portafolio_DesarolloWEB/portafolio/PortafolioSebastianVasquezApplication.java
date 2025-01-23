f04413af67e3b292be6b279a1d506c234a9b4ff9
Sebastian Vasquez <svasvil0211@gmail.com>
2025-01-22 20:55:18 -0600

    Commit Semana2
    Commit semana 2

---
+++ Portafolio_Sebastian_Vasquez/src/main/java/Portafolio_DesarolloWEB/portafolio/PortafolioSebastianVasquezApplication.java
@@ -0,0 +1,13 @@
+package Portafolio_DesarolloWEB.portafolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioSebastianVasquezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioSebastianVasquezApplication.class, args);
+	}
+
+}
---
+++ Portafolio_Sebastian_Vasquez/src/test/java/Portafolio_DesarolloWEB/portafolio/PortafolioSebastianVasquezApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_DesarolloWEB.portafolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioSebastianVasquezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
